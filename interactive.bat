c69df94048f1b953e066d090502c395e3de6bcd2
nullprop <git@nullprop.sh>
2022-07-28 15:31:56 +0300

    win script

---
+++ interactive.bat
@@ -0,0 +1,8 @@
+docker build -t qlsb .
+docker run -it ^
+    -p 27961:27961 ^
+    -p 27961:27961/udp ^
+    -p 28961:28961 ^
+    -v qlsb_steamapps_vol:/home/steam/qlds/steamapps ^
+    -v qlsb_redis_vol:/var/lib/redis ^
+    --cap-add=SYS_NICE qlsb
